3ce1dd81ae46f4ce2d21334dc2c862a3f2d662b1
BassaniDavid <davbassan98@gmail.com>
2025-02-26 14:56:29 +0100

    feat: 5th bonus exercise complete

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -21,6 +21,7 @@ console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
+
 const longNames = [];
 
 for (let i=0; i<teachers.length; i++){
@@ -43,6 +44,7 @@ console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
+
 let yesornot = 'false';
 
 for (let i=0; i<teachers.length; i++){
@@ -55,4 +57,12 @@ const isFabioPresent = yesornot;
 console.log(isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+
+let allTeachers = '';
+
+for (let i=0; i<teachers.length; i++){
+  allTeachers = allTeachers + teachers[i] + ','
+};
+
+const teachersString = allTeachers;
+console.log(teachersString);
